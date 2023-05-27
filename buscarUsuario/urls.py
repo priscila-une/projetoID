d42527cc199c42e34036cfba922e68b6327a82c7
Paulo Vieira <paulo.nfv@gmail.com>
2023-05-27 17:52:36 -0300

    projetoid

--- buscarUsuario/urls.py
+++ buscarUsuario/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
-from buscarUsuario.views import buscarUsuario
+from buscarUsuario.views import buscarUsuario, credencial, exclui_usuario
 
 urlpatterns = [
     path('pesquisa', buscarUsuario, name='pesquisa'),
     path('buscarUsuario/', buscarUsuario, name='buscar_usuario'),
+
+    path('credencial/<int:func_id>/', credencial, name='gera_credencial'),
+    path('credencial', credencial, name='credencial'),
+
+    path('exclui_usuario/<int:func_id>/', exclui_usuario, name='exclui_usuario'),
 ]
--- buscarUsuario/views.py
+++ buscarUsuario/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .forms import SearchForm
+from django.contrib import messages
+from django.db.models import Q
 from criarConta.models import CustomUser #trocar depois pela tabela dos usuários
 
 def buscarUsuario(request):
@@ -10,6 +12,20 @@ def buscarUsuario(request):
         form = SearchForm(request.GET)
         if form.is_valid():
             consulta = form.cleaned_data['consulta']
-            results = CustomUser.objects.filter(nome_completo__icontains=consulta)
+            request.session['input_value'] = consulta
+            results = CustomUser.objects.filter(Q(nome_completo__icontains=consulta) | Q(registro_funcional=consulta))
+            results = results.order_by('nome_completo')
+            form = SearchForm(initial={'input_field': request.session.get('input_value', '')})
 
     return render(request, 'buscarUsuario/buscar_usuario.html', {'form': form, 'results': results})
+
+def credencial(request, func_id):
+    usuario = get_object_or_404(CustomUser, idfunc=func_id)
+    # context: {'registro': registro } permite que o parâmetro seja passado para o template credencial.html
+    return render (request, 'credencial/credencial.html', {'usuario': usuario })
+
+def exclui_usuario(request, func_id):
+    usuario = get_object_or_404(CustomUser, idfunc=func_id)
+    usuario.delete()
+    messages.success(request, 'Usuário excluído com sucesso.')
+    return render (request, 'buscarUsuario/buscar_usuario.html', {'usuario': usuario })
